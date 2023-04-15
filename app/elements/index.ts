edbf9620d55498d0150f99db60d8b6d86417f43d
unknown <krzys.repsch@gmail.com>
2023-04-15 18:24:53 +0200

    feat: add Info component

--- app/elements/index.ts
+++ app/elements/index.ts
@@ -1,5 +1,6 @@
 export { default as Button } from './Button';
 export { default as Header } from './Header';
+export { default as Info } from './Info';
 export { default as Link } from './Link';
 export { default as PolaroidImage } from './PolaroidImage';
 export { default as Popover } from './Popover';
