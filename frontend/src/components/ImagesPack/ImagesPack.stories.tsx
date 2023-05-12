50435b6abf31975b09cf6034aa1802e5a1b1ecd6
unknown <krzys.repsch@gmail.com>
2023-05-12 19:40:55 +0200

    feat: fix ImagesPack story

--- frontend/src/components/ImagesPack/ImagesPack.stories.tsx
+++ frontend/src/components/ImagesPack/ImagesPack.stories.tsx
@@ -1,7 +1,10 @@
 /**
  * External dependencies
  */
-import { StoryObj, Meta, StoryFn } from '@storybook/react';
+import { ArgsStoryFn } from '@storybook/types';
+import { ReactRenderer, StoryObj, Meta } from '@storybook/react';
+import { useRef } from 'react';
+import { ComponentProps } from 'react';
 
 /**
  * Internal dependencies
@@ -9,44 +12,49 @@ import { StoryObj, Meta, StoryFn } from '@storybook/react';
 import DollImage1 from '@/images/example-doll-1.png';
 import DollImage2 from '@/images/example-doll-2.png';
 import DollImage3 from '@/images/example-doll-3.png';
-import ImagesPack from '.';
+import ImagesPack from './ImagesPack';
 
 export default {
 	title: 'Components/ImagesPack',
 	component: ImagesPack,
-	decorators: [
-		(Story: StoryFn) => (
-			<div
-				style={{
-					maxWidth: '49.625rem',
-					height: '49.625rem',
-				}}
-			>
-				<Story />
-			</div>
-		),
-	],
-	excludeStories: ['DefaultProps'],
 } satisfies Meta<typeof ImagesPack>;
 
-export const DefaultProps = {
-	animate: true,
-	images: [
-		{
-			src: DollImage1.src,
-			alt: '',
-		},
-		{
-			src: DollImage2.src,
-			alt: '',
-		},
-		{
-			src: DollImage3.src,
-			alt: '',
-		},
-	],
+const ImagesPackStory: ArgsStoryFn<
+	ReactRenderer,
+	ComponentProps<typeof ImagesPack>
+> = (args) => {
+	const parentRef = useRef<HTMLDivElement>(null);
+
+	return (
+		<div
+			ref={parentRef}
+			style={{
+				maxWidth: '49.625rem',
+				height: '49.625rem',
+			}}
+		>
+			<ImagesPack {...args} parentRef={parentRef} />
+		</div>
+	);
 };
 
 export const Default: StoryObj<typeof ImagesPack> = {
-	args: DefaultProps,
+	render: ImagesPackStory,
+	args: {
+		animate: true,
+		images: [
+			{
+				src: DollImage1.src,
+				alt: '',
+			},
+			{
+				src: DollImage2.src,
+				alt: '',
+			},
+			{
+				src: DollImage3.src,
+				alt: '',
+			},
+		],
+	},
 };
