7935db47eea2fa707ad385b47da4be5080ce71a5
unknown <krzys.repsch@gmail.com>
2023-05-14 22:53:14 +0200

    t

--- frontend/src/utils/markProductAsSold.ts
+++ frontend/src/utils/markProductAsSold.ts
@@ -1,6 +1,6 @@
 const markProductAsSold = async (slug: string) => {
 	const findProduct = await fetch(
-		`${process.env.NEXT_PUBLIC_DATABASE_URL}/api/dolls?filters[slug][$eq]=${slug}`
+		`https://charming-dolls.herokuapp.com/api/dolls?filters[slug][$eq]=${slug}`
 	);
 
 	const data = await findProduct.json();
@@ -11,11 +11,11 @@ const markProductAsSold = async (slug: string) => {
 	productData.attributes.isSold = !isSold;
 
 	await fetch(
-		`${process.env.NEXT_PUBLIC_DATABASE_URL}/api/dolls/${productData.id}`,
+		`https://charming-dolls.herokuapp.com/api/dolls/${productData.id}`,
 		{
 			method: 'PUT',
 			headers: {
-				Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_TOKEN}`,
+				Authorization: `Bearer 037f77ab00f840a1675552ee54aa4fb8547fce1f46863de8102727caa2289080ae7c5f1a7ec3ab4278b5c5502a23f805b941a5554cf89237f49e7de93cc8d3e92bfa31de5843da4116892ec70b51f210c1b2c7450590db0da7a723913c229ea324f7033ad3a47d6ba2e6052db5414be69a2210a45088cc98f4b5742e90cdbfaf`,
 				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify({
@@ -23,6 +23,8 @@ const markProductAsSold = async (slug: string) => {
 			}),
 		}
 	);
+
+	return productData;
 };
 
 export default markProductAsSold;
