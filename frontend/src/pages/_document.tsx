04118973f50a4a985907d11647f6f1416b5db869
unknown <krzys.repsch@gmail.com>
2023-05-14 17:35:49 +0200

    feat(frontend): add fonts directly in Head

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -5,7 +5,14 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 const Document = () => (
 	<Html>
-		<Head />
+		<Head>
+			<link rel="preconnect" href="https://fonts.googleapis.com" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" />
+			<link
+				href="https://fonts.googleapis.com/css2?family=Dosis:wght@400;600;700&family=Pacifico&display=swap"
+				rel="stylesheet"
+			/>
+		</Head>
 		<body>
 			<Main />
 			<NextScript />
