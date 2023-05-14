de5f3fee72854baec4583bf6c62aa9572685e871
unknown <krzys.repsch@gmail.com>
2023-05-14 21:46:46 +0200

    3

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,9 +2,11 @@
  * Internal dependencies
  */
 import { AboutMeLayout, WithNavigationLayout } from '@/layouts';
+import { markProductAsSold } from '@/utils';
 
 const Doll = () => (
 	<WithNavigationLayout>
+		<button onClick={() => markProductAsSold('nora')}>sdasddsa</button>
 		<AboutMeLayout />
 	</WithNavigationLayout>
 );
