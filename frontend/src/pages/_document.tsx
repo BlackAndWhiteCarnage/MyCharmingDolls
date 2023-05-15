060f0b683519ab0d4f8a6722fe989b80af1024ba
unknown <krzys.repsch@gmail.com>
2023-05-15 15:35:47 +0200

    feat(frontend): add favicon

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -8,6 +8,7 @@ const Document = () => (
 		<Head>
 			<link rel="preconnect" href="https://fonts.googleapis.com" />
 			<link rel="preconnect" href="https://fonts.gstatic.com" />
+			<link rel="shortcut icon" href="/favicon.svg" />
 			<link
 				href="https://fonts.googleapis.com/css2?family=Dosis:wght@400;600;700&family=Pacifico&display=swap"
 				rel="stylesheet"
