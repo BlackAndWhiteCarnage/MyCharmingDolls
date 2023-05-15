5e3f9dbc3190013824d59ed039d37dd40bdbe455
unknown <krzys.repsch@gmail.com>
2023-05-15 18:07:17 +0200

    faet(frontend): update afterPaymentContent

--- frontend/src/config/afterPaymentContent.ts
+++ frontend/src/config/afterPaymentContent.ts
@@ -4,9 +4,9 @@
 import { formatPrice } from '@/utils';
 
 export const canceledContent = `
-I regret to inform you that your order has been canceled. I want to personally express my sincere apologies for the cancellation of your order. I understand that this may be disappointing, and I apologize for any inconvenience caused. If you have any questions or require further assistance, please feel free to reach out to me.
+I regret to inform you that your order has been canceled. If you have any questions or require further assistance, please feel free to reach out to me via contact form.
 
-Although your current order couldn't be fulfilled, I invite you to explore my collection of other dolls available for adoption. I take pride in offering a diverse range of beautiful and unique dolls, each waiting for a loving home. Take a moment to browse through the selection, and you might just find the perfect companion.
+Although your current order couldn't be fulfilled, I invite you to explore my collection of other dolls available for adoption. I take pride in offering a diverse range of beautiful and unique Blythe dolls, each waiting for a loving home. Take a moment to browse through the selection, and you might just find the perfect companion.
                         
 I appreciate your support, and I hope to have the opportunity to provide you with a better experience in the future.
                         
@@ -34,7 +34,7 @@ Your purchase has helped support my small business and allowed me to continue cr
                             
 An email confirmation will be automatically sent to **${email}** for your reference.
                             
-Once again, thank you for adopting one of my dolls. If you have any questions or concerns, please feel free to contact me. I hope your new companion brings you many years of happiness.
+Once again, thank you for adopting one of my dolls. If you have any questions or concerns, please feel free to contact me via contact form. I hope your new companion brings you many years of happiness.
                
 **Best regards,**
                             
