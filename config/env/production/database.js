db170c557bbb7235145d10974178323b79961b39
unknown <krzys.repsch@gmail.com>
2023-05-16 14:06:18 +0200

    dasdas

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -12,14 +12,9 @@ module.exports = ({ env }) => {
         database,
         user,
         password,
-        ssl: {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-        },  
+        ssl: { rejectUnauthorized: false },
       },
-    debug: false,
-    options: {
-      ssl: env.bool('DATABASE_SSL', false),
-    },
+      debug: false,
     },
   }
 };
