618f9ce4b720ff76a7dcca124ca77bfa7214aaf5
unknown <krzys.repsch@gmail.com>
2023-05-16 14:23:10 +0200

    cxcx

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,14 @@
 module.exports = ({ env }) => ({
+    proxy: true,
+    host: "0.0.0.0",
+    port: process.env.PORT || 4000,
     url: env('MY_HEROKU_URL'),
-  });
+    app: { 
+      keys: env.array('APP_KEYS')
+    },
+    admin: {
+      auth: {
+        secret: env('ADMIN_JWT_SECRET'),
+      },
+    },
+  })
