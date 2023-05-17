4a9385258dd66b822ed3c81f12e594017138587d
unknown <krzys.repsch@gmail.com>
2023-05-17 17:10:44 +0200

    fix(frontend): Textarea story

--- frontend/src/components/Textarea/Textarea.stories.tsx
+++ frontend/src/components/Textarea/Textarea.stories.tsx
@@ -1,16 +1,24 @@
 /**
  * External dependencies
  */
-import { StoryObj, Meta } from '@storybook/react';
+import { StoryObj, Meta, StoryFn } from '@storybook/react';
 
 /**
  * Internal dependencies
  */
+import { Form } from '@/components';
 import Textarea from './Textarea';
 
 export default {
 	title: 'Components/Textarea',
 	component: Textarea,
+	decorators: [
+		(Story: StoryFn) => (
+			<Form>
+				<Story />
+			</Form>
+		),
+	],
 } satisfies Meta<typeof Textarea>;
 
 export const Default: StoryObj<typeof Textarea> = {
